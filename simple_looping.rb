fb7b210c2b2a6d1add0fa75a698be308c58e5f49
Rob Parker <into.the.parker@gmail.com>
2018-08-19 19:50:40 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -60,7 +60,7 @@ def for_iterator(number_of_times)
 
   phrase = "Welcome to Flatiron School's Web Development Course!"
 
-  for poop in number_of_times do
+  for counter in number_of_times do
     puts phrase
   end
 
